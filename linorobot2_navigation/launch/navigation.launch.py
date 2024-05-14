b09e83213c239cc9cd19892fc9e3b9655a894213
Jacopopsen <jacob57@live.dk>
2024-05-14 10:36:26 +0200

    .

--- linorobot2_navigation/launch/navigation.launch.py
+++ linorobot2_navigation/launch/navigation.launch.py
@@ -21,7 +21,7 @@ from launch.conditions import IfCondition
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
-MAP_NAME='grupperum' #change to the name of your own map here
+MAP_NAME='gas_station' #change to the name of your own map here
 
 def generate_launch_description():
 
